54364e158f824459755c659fadf8e6f9d6063e57
raulmartinez <raulmartinez@paradigmadigital.com>
2025-06-02 19:15:29 +0200

    Ajustes frontal

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,10 +12,7 @@ function Navbar() {
 
   return (
     <nav style={{ padding: "1rem", background: "#1976d2", display: 'flex', alignItems: 'center' }}>
-      <Link style={{ color: "#fff", marginRight: 16 }} to="/catalog">Catalog</Link>
-      <Link style={{ color: "#fff", marginRight: 16 }} to="/checkout">Checkout</Link>
-      <Link style={{ color: "#fff", marginRight: 16 }} to="/payment">Payment</Link>
-      <Link style={{ color: "#fff", marginRight: 16 }} to="/confirmation">Confirmation</Link>
+
       <div style={{ flex: 1 }} />
       <Link to="/cart">
         <IconButton sx={{ color: '#fff' }}>
