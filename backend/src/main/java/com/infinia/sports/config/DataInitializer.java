62dd2e4ce25d985409a87e9227ed7dad8f0fb4b1
raulmartinez <raulmartinez@paradigmadigital.com>
2025-06-13 13:00:14 +0200

    Metemos imagenes generadas con IA

--- backend/src/main/java/com/infinia/sports/config/DataInitializer.java
+++ backend/src/main/java/com/infinia/sports/config/DataInitializer.java
@@ -42,35 +42,35 @@ public class DataInitializer {
                 product1.setDescription("Nike Air Max 90");
                 product1.setPrice(new BigDecimal("129.99"));
                 product1.setSize("42");
-                product1.setImageUrl("nike_air_max_90.svg");
+                product1.setImageUrl("nike_air_max_90.jpg");
 
                 Product product2 = new Product();
                 product2.setType(ProductType.SNEAKERS);
                 product2.setDescription("Adidas Ultraboost");
                 product2.setPrice(new BigDecimal("159.99"));
                 product2.setSize("43");
-                product2.setImageUrl("adidas_ultraboost.svg");
+                product2.setImageUrl("adidas_ultraboost.jpg");
 
                 Product product3 = new Product();
                 product3.setType(ProductType.CLOTHING);
                 product3.setDescription("Camiseta Nike Dri-FIT");
                 product3.setPrice(new BigDecimal("34.99"));
                 product3.setSize("M");
-                product3.setImageUrl("nike_dri_fit_shirt.svg");
+                product3.setImageUrl("nike_dri_fit_shirt.jpg");
 
                 Product product4 = new Product();
                 product4.setType(ProductType.CLOTHING);
                 product4.setDescription("Pantalón Adidas Training");
                 product4.setPrice(new BigDecimal("49.99"));
                 product4.setSize("L");
-                product4.setImageUrl("adidas_training_pants.svg");
+                product4.setImageUrl("adidas_training_pants.jpg");
 
                 Product product5 = new Product();
                 product5.setType(ProductType.SUPPLEMENT);
                 product5.setDescription("Proteína Whey Gold Standard");
                 product5.setPrice(new BigDecimal("29.99"));
                 product5.setSize("900g");
-                product5.setImageUrl("whey_gold_standard.svg");
+                product5.setImageUrl("whey_gold_standard.jpg");
 
                 // Productos sintéticos adicionales para pruebas de paginación
                 Product product6 = new Product();
@@ -78,105 +78,105 @@ public class DataInitializer {
                 product6.setDescription("Puma Velocity Nitro 2");
                 product6.setPrice(new BigDecimal("119.99"));
                 product6.setSize("41");
-                product6.setImageUrl("puma_velocity_nitro_2.svg");
+                product6.setImageUrl("puma_velocity_nitro_2.jpg");
 
                 Product product7 = new Product();
                 product7.setType(ProductType.SNEAKERS);
                 product7.setDescription("New Balance Fresh Foam 1080");
                 product7.setPrice(new BigDecimal("139.99"));
                 product7.setSize("44");
-                product7.setImageUrl("nb_fresh_foam_1080.svg");
+                product7.setImageUrl("nb_fresh_foam_1080.jpg");
 
                 Product product8 = new Product();
                 product8.setType(ProductType.CLOTHING);
                 product8.setDescription("Shorts Under Armour Training");
                 product8.setPrice(new BigDecimal("24.99"));
                 product8.setSize("L");
-                product8.setImageUrl("ua_training_shorts.svg");
+                product8.setImageUrl("ua_training_shorts.jpg");
 
                 Product product9 = new Product();
                 product9.setType(ProductType.CLOTHING);
                 product9.setDescription("Sudadera Nike Therma-FIT");
                 product9.setPrice(new BigDecimal("54.99"));
                 product9.setSize("XL");
-                product9.setImageUrl("nike_therma_fit_hoodie.svg");
+                product9.setImageUrl("nike_therma_fit_hoodie.jpg");
 
                 Product product10 = new Product();
                 product10.setType(ProductType.SUPPLEMENT);
                 product10.setDescription("Creatina Monohidrato 500g");
                 product10.setPrice(new BigDecimal("19.99"));
                 product10.setSize("500g");
-                product10.setImageUrl("creatina_monohidrato.svg");
+                product10.setImageUrl("creatina_monohidrato.jpg");
 
                 Product product11 = new Product();
                 product11.setType(ProductType.SUPPLEMENT);
                 product11.setDescription("Barrita Proteica Chocolate");
                 product11.setPrice(new BigDecimal("2.99"));
                 product11.setSize("50g");
-                product11.setImageUrl("barrita_chocolate.svg");
+                product11.setImageUrl("barrita_chocolate.jpg");
 
                 Product product12 = new Product();
                 product12.setType(ProductType.CLOTHING);
                 product12.setDescription("Calcetines Deportivos Pack x3");
                 product12.setPrice(new BigDecimal("9.99"));
                 product12.setSize("M");
-                product12.setImageUrl("pack_calcetines.svg");
+                product12.setImageUrl("pack_calcetines.jpg");
 
                 Product product13 = new Product();
                 product13.setType(ProductType.SNEAKERS);
                 product13.setDescription("Asics Gel-Kayano 28");
                 product13.setPrice(new BigDecimal("149.99"));
                 product13.setSize("42");
-                product13.setImageUrl("asics_gel_kayano_28.svg");
+                product13.setImageUrl("asics_gel_kayano_28.jpg");
 
                 Product product14 = new Product();
                 product14.setType(ProductType.SUPPLEMENT);
                 product14.setDescription("BCAA 2:1:1 300g");
                 product14.setPrice(new BigDecimal("15.99"));
                 product14.setSize("300g");
-                product14.setImageUrl("bcaa_211.svg");
+                product14.setImageUrl("bcaa_211.jpg");
 
                 Product product15 = new Product();
                 product15.setType(ProductType.CLOTHING);
                 product15.setDescription("Mallas Running Mujer");
                 product15.setPrice(new BigDecimal("39.99"));
                 product15.setSize("S");
-                product15.setImageUrl("mallas_running_mujer.svg");
+                product15.setImageUrl("mallas_running_mujer.jpg");
 
                 Product product16 = new Product();
                 product16.setType(ProductType.SNEAKERS);
                 product16.setDescription("Reebok Nano X2");
                 product16.setPrice(new BigDecimal("129.99"));
                 product16.setSize("43");
-                product16.setImageUrl("reebok_nano_x2.svg");
+                product16.setImageUrl("reebok_nano_x2.jpg");
 
                 Product product17 = new Product();
                 product17.setType(ProductType.SUPPLEMENT);
                 product17.setDescription("Pre-entreno Energy Shot");
                 product17.setPrice(new BigDecimal("3.99"));
                 product17.setSize("60ml");
-                product17.setImageUrl("energy_shot.svg");
+                product17.setImageUrl("energy_shot.jpg");
 
                 Product product18 = new Product();
                 product18.setType(ProductType.CLOTHING);
                 product18.setDescription("Camiseta Adidas Aeroready");
                 product18.setPrice(new BigDecimal("29.99"));
                 product18.setSize("L");
-                product18.setImageUrl("adidas_aeroready_shirt.svg");
+                product18.setImageUrl("adidas_aeroready_shirt.jpg");
 
                 Product product19 = new Product();
                 product19.setType(ProductType.SNEAKERS);
                 product19.setDescription("Mizuno Wave Rider 25");
                 product19.setPrice(new BigDecimal("134.99"));
                 product19.setSize("41");
-                product19.setImageUrl("mizuno_wave_rider_25.svg");
+                product19.setImageUrl("mizuno_wave_rider_25.jpg");
 
                 Product product20 = new Product();
                 product20.setType(ProductType.SUPPLEMENT);
                 product20.setDescription("Proteína Vegana 750g");
                 product20.setPrice(new BigDecimal("32.99"));
                 product20.setSize("750g");
-                product20.setImageUrl("proteina_vegana.svg");
+                product20.setImageUrl("proteina_vegana.jpg");
 
                 // Guardar productos en la base de datos
                 productRepository.saveAll(Arrays.asList(
