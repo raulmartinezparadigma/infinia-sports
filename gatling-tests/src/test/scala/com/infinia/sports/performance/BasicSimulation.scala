0f25664de389170239cb5696188fb46fd75be44a
raulmartinez <raulmartinez@paradigmadigital.com>
2025-07-04 21:13:03 +0200

    Nuevo modulo para pruebas de rendimiento

---
+++ gatling-tests/src/test/scala/com/infinia/sports/performance/BasicSimulation.scala
@@ -0,0 +1,23 @@
+package com.infinia.sports.performance
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8081") // Asegúrate de que este es el puerto correcto de tu aplicación Spring Boot
+    .acceptHeader("application/json")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Gatling/3.9.5")
+
+  val scn = scenario("Get All Products")
+    .exec(http("request_products")
+      .get("/api/products"))
+    .pause(1)
+
+  setUp(
+    scn.inject(atOnceUsers(10)) // Simula 10 usuarios concurrentes
+  ).protocols(httpProtocol)
+}
